621a7b6f0ac6f3c0ef45067f7e0b308ce00f93cd
Jose-An608 <jvilcanquic@unjbg.edu.pe>
2026-05-12 02:01:20 -0500

    Probando comando INSERT

---
+++ PruebaMySQL/Insert.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Alumnos(id_alumno, nombre_alumno,edad,materia,grado)
+VALUES(1, 'Juan Lopez', 22, 'Matematicas',4),(2, 'Marcos Perez',21,'Quimica', 3),
+(3, 'Ana Sanchez', 35, 'Fisica', 4);
+
+SELECT*FROM Alumnos;
